f2836d207be2a72716ec030dba6377783a3c4289
avempaceVoice <voice-dev@avempace-wireless.com>
2017-06-15 10:02:50 +0100

    commit of demo working copy

---
+++ app.js
@@ -0,0 +1,133 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var engine = require('ejs-locals');
+var routes = require('./routes/index');
+var helmet = require('helmet')
+var users = require('./routes/users');
+var session = require('express-session');
+var request = require('request');
+var passport = require('passport');
+var flash = require('connect-flash');
+var req = require('request');
+var cookie = require("cookie");
+var sessionStorage = require('express-mysql-session');
+var config = require('./config/dbconfig.json');
+require('./config/passport')(passport);
+//var flash    = require('connect-flash');
+
+/**
+ *
+ * session storage configuration for postgres database
+ * to do this we have to execute table.sql in our database
+ * table.sql is in the connnect-pg-simple module
+ *
+ */
+//var pg= require('pg');
+//var StoreSession = require('connect-pg-simple')(session);
+
+
+/**
+ *
+ * initialize express
+ * @type {*|exports}
+ */
+var app = express();
+
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet())
+app.use(session({ secret: 'a4f8071f-c873-4447-8ee2', name: 'nsIdForsession', saveUninitialized: true, resave: true }));
+
+/**
+ * Passport config
+ *
+ */
+
+
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+app.use(function(req, res, next) {
+
+
+    next();
+});
+
+/**
+ *
+ * route configuration
+ */
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.use(function(req, res, next) {
+
+    console.log('verifying identity')
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log(fullUrl)
+
+    if (fullUrl == 'http://www.baidu.com/cache/global/img/gs.gif' || fullUrl == 'http://www.baidu.comhttp://www.baidu.com/cache/global/img/gs.gif') {
+
+        console.log('intrusion try')
+        res.end()
+    } else {
+        next()
+    }
+
+
+})
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+
+    res.locals.user = null;
+    res.send('error  ');
+
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        console.log('error log ', err)
+        res.status(err.status || 500);
+
+        res.locals.user = null;
+        res.send('error erreur');
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+
+    res.locals.user = null;
+    res.send('error');
+});
+
+
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,576 @@
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+var lockService = require('../services/lockService')
+var list = ['living room', 'arpegio']
+var fn = require('../bin/www')
+var io = require('socket.io');
+var reqhttp = require('request')
+
+/* GET home page. */
+router.get('/', securityCheck, function(req, res, next) {
+    var tab = []
+    fn.clients.forEach(function(soc) {
+        tab.push(soc.name)
+    })
+
+    res.json({ list: tab })
+
+});
+
+router.post('/unlinkspeaker', function(req, res, next) {
+    fn.clients.forEach(function(soc) {
+        if (soc.name == req.body.key) {
+            console.log('theire is a socket named as requested', soc.name)
+            reqhttp.post('https://oauth20.herokuapp.com/api/updateSpeakerByNumSerie', { form: { linked: false, num_serie: soc.name } }, function(result) {
+                console.log(result)
+            })
+            soc.linked = false
+            var index = fn.lastSelectedClient.indexOf(soc.name)
+            fn.lastSelectedClient.splice(index, 1)
+            res.end()
+
+
+
+
+
+        }
+    })
+
+})
+
+router.post('/', securityCheck, function(req, res, next) {
+    console.log('speaker to link ', req.body.key)
+    console.log('length of table socket  ', fn.clients.length)
+    ctr = 0
+    i = 0
+    if (fn.clients.length == 0) {
+        res.send('not found')
+    } else {
+        fn.clients.forEach(function(soc) {
+            if (soc.name == req.body.key) {
+                console.log('theire is a socket named as requested', soc.name)
+                reqhttp.post('https://oauth20.herokuapp.com/api/updateSpeakerByNumSerie', { form: { linked: true, num_serie: soc.name } }, function(result) {
+                    console.log(result)
+                })
+                soc.linked = true
+                fn.lastSelectedClient.push(soc.name)
+                i = 1
+
+
+
+
+
+            }
+
+
+
+
+            ctr++;
+            if (ctr === fn.clients.length) {
+                if (i == 0) {
+                    res.send('not found')
+                } else {
+                    return res.send('found')
+                }
+
+            }
+
+
+
+
+        })
+
+    }
+
+
+
+})
+
+
+router.post('/checkIfConnected', securityCheck, function(re, res, next) {
+    var numSerie = req.body.key;
+    var i = 0;
+    fn.clients.forEach(function(soc) {
+
+        if (soc.name == numSerie) {
+            i = 1
+            res.send(true)
+        }
+    })
+    if (i == 0) {
+        res.send(false)
+    }
+
+
+
+})
+
+router.post('/checkIfSelected', securityCheck, function(re, res, next) {
+    var numSerie = req.body.key;
+    var i = 0;
+    fn.clients.forEach(function(soc) {
+
+        if (soc.name == numSerie && soc.linked == true) {
+            i = 1
+            res.send(true)
+        }
+    })
+    if (i == 0) {
+        res.send(false)
+    }
+
+
+
+})
+
+router.get('/getConnectedDevice', securityCheck, function(req, res, next) {
+    var i = 0
+    fn.clients.forEach(function(soc) {
+        if (soc.linked == true) {
+            i = 1
+            res.send(soc.name)
+        }
+    })
+
+
+    if (i == 0) {
+        res.send(false)
+    }
+
+
+})
+
+router.post('/linktoanyone', securityCheck, function(req, res, next) {
+    console.log('inside link to any one');
+    console.log(req.body.key)
+    console.log(fn.clients.length)
+
+    if (fn.clients.length == 0) {
+        console.log('inside if pas de client')
+        res.send('error')
+    } else {
+        console.log('inside if theire is a client')
+        fn.clients[0].linked = true
+        res.send(fn.clients[0].name)
+    }
+
+
+
+
+})
+
+router.post('/playnext', securityCheck, function(req, res, next) {
+    console.log('req.body.key', req.body.key)
+
+    if (!req.body.key) {
+
+
+        fn.clients.forEach(function(soc) { // browse all speaker connected
+
+
+
+
+            if (soc.linked == true) {
+
+                fn.sendSocketToSpeaker(soc.name, 'play_next', function(result) {
+
+
+
+                    if (result != false) {
+
+
+                        res.send({ status: 'ok' })
+                    } else {
+
+                        res.send({ status: 'no' })
+                    }
+
+
+                })
+
+            }
+
+        })
+
+        res.send({ status: 'no' })
+
+
+
+
+    } else {
+        if (fn.clients.length == 0) {
+            res.send({ status: 'no' })
+        } else {
+            fn.sendSocketToSpeaker(req.body.key, 'play_next', function(result) {
+                if (result != false) {
+
+                    res.send({ status: 'ok' })
+                } else {
+
+                    res.send({ status: 'no' })
+                }
+
+
+            })
+
+
+        }
+    }
+
+
+})
+
+router.post('/playprevious', securityCheck, function(req, res, next) {
+    console.log('req.body.key', req.body.key)
+    if (!req.body.key) {
+
+
+        fn.clients.forEach(function(soc) {
+
+
+            if (soc.linked == true) {
+
+                fn.sendSocketToSpeaker(soc.name, 'play_prev', function(result) {
+                    console.log(result)
+                    if (result != false) {
+                        res.send({ status: 'ok' })
+                        res.end()
+
+                    } else {
+                        res.send({ status: 'no' })
+                        res.end()
+                    }
+
+
+                })
+
+            }
+
+        })
+        res.send({ status: 'no' })
+
+    } else {
+        if (fn.clients.length == 0) {
+            res.send({ status: 'no' })
+        } else {
+            fn.sendSocketToSpeaker(req.body.key, 'play_prev', function(result) {
+                console.log(result)
+                if (result != false) {
+                    res.send({ status: 'ok' })
+                } else {
+                    res.send({ status: 'no' })
+                }
+
+            })
+
+
+        }
+    }
+
+
+})
+
+router.post('/playtrack', securityCheck, function(req, res, next) {
+    console.log('req.body.key', req.body.key)
+    if (!req.body.key) {
+
+
+        fn.clients.forEach(function(soc) {
+
+
+            if (soc.linked == true) {
+
+                fn.sendSocketToSpeaker(soc.name, 'play', function(result) {
+                    if (result != false) {
+                        res.send({ status: 'ok' })
+                    } else {
+                        res.send({ status: 'no' })
+                    }
+
+
+                })
+
+            }
+
+        })
+        res.send({ status: 'no' })
+    } else {
+        if (fn.clients.length == 0) {
+            res.send({ status: 'no' })
+        } else {
+            fn.sendSocketToSpeaker(req.body.key, 'play', function(result) {
+                if (result != false) {
+                    res.send({ status: 'ok' })
+                } else {
+                    res.send({ status: 'no' })
+                }
+
+
+            })
+
+
+        }
+    }
+
+
+})
+
+router.post('/increasevolume', securityCheck, function(req, res, next) {
+    console.log('req.body.key', req.body.key)
+    valtoIncrease = req.body.nb
+    if (!req.body.key) {
+
+
+        fn.clients.forEach(function(soc) {
+
+
+            if (soc.linked == true) {
+
+                fn.sendSocketToSpeaker(soc.name, 'volume_increase:' + valtoIncrease, function(result) {
+                    if (result != false) {
+                        res.send({ status: 'ok' })
+                    } else {
+                        res.send({ status: 'no' })
+                    }
+
+
+                })
+
+            }
+
+        })
+        res.send({ status: 'no' })
+
+    } else {
+        if (fn.clients.length == 0) {
+            res.send({ status: 'no' })
+        } else {
+            fn.sendSocketToSpeaker(req.body.key, 'volume_increase:' + valtoIncrease, function(result) {
+                if (result != false) {
+                    res.send({ status: 'ok' })
+                } else {
+                    res.send({ status: 'no' })
+                }
+
+
+            })
+
+
+        }
+    }
+
+
+})
+
+router.post('/incrvolume', securityCheck, function(req, res, next) {
+
+
+
+    if (!req.body.key) {
+
+
+        fn.clients.forEach(function(soc) {
+
+
+            if (soc.linked == true) {
+
+                fn.sendSocketToSpeaker(soc.name, 'volume_increase', function(result) {
+                    if (result != false) {
+                        res.send({ status: 'ok' })
+                    } else {
+                        res.send({ status: 'no' })
+                    }
+
+
+                })
+
+            }
+
+        })
+        res.send({ status: 'no' })
+
+    } else {
+
+
+
+        if (fn.clients.length == 0) {
+            res.send({ status: 'no' })
+        } else {
+            if (fn.clients.length == 0) {
+                res.send({ status: 'no' })
+            } else {
+                fn.sendSocketToSpeaker(req.body.key, 'volume_increase', function(err, result) {
+                    console.log(err)
+                    if (result != false) {
+                        res.send({ status: 'ok' })
+                    } else {
+                        res.send({ status: 'no' })
+                    }
+
+
+                })
+            }
+        }
+
+
+
+    }
+
+
+})
+
+router.post('/decreasevolume', securityCheck, function(req, res, next) {
+    valtoDecrease = req.body.nb
+    console.log('req.body.key', req.body.key)
+    if (!req.body.key) {
+
+
+        fn.clients.forEach(function(soc) {
+
+
+            if (soc.linked == true) {
+
+                fn.sendSocketToSpeaker(soc.name, 'volume_decrease:' + valtoDecrease, function(result) {
+                    if (result != false) {
+                        res.send({ status: 'ok' })
+                    } else {
+                        res.send({ status: 'no' })
+                    }
+
+
+                })
+
+            }
+
+        })
+        res.send({ status: 'no' })
+    } else {
+        if (fn.clients.length == 0) {
+            res.send({ status: 'no' })
+        } else {
+            fn.sendSocketToSpeaker(req.body.key, 'volume_decrease:' + valtoDecrease, function(result) {
+                if (result != false) {
+                    res.send({ status: 'ok' })
+                } else {
+                    res.send({ status: 'no' })
+                }
+
+
+            })
+
+        }
+    }
+
+
+})
+
+
+router.post('/decrevolume', securityCheck, function(req, res, next) {
+
+    console.log('req.body.key', req.body.key)
+    if (!req.body.key) {
+
+
+        fn.clients.forEach(function(soc) {
+
+
+            if (soc.linked == true) {
+
+                fn.sendSocketToSpeaker(soc.name, 'volume_decrease', function(result) {
+                    if (result != false) {
+                        res.send({ status: 'ok' })
+                    } else {
+                        res.send({ status: 'no' })
+                    }
+
+
+                })
+
+            }
+
+        })
+        res.send({ status: 'no' })
+    } else {
+        if (fn.clients.length == 0) {
+            res.send({ status: 'no' })
+        } else {
+            fn.sendSocketToSpeaker(req.body.key, 'volume_decrease', function(result) {
+                if (result != false) {
+                    res.send({ status: 'ok' })
+                } else {
+                    res.send({ status: 'no' })
+                }
+
+
+            })
+        }
+
+    }
+
+
+})
+
+
+router.post('/pause', securityCheck, function(req, res, next) {
+    console.log('req.body.key', req.body.key)
+    if (!req.body.key) {
+
+
+        fn.clients.forEach(function(soc) {
+
+
+            if (soc.linked == true) {
+
+                fn.sendSocketToSpeaker(soc.name, 'pause_toggle', function(result) {
+                    if (result != false) {
+                        res.send({ status: 'ok' })
+                    } else {
+                        res.send({ status: 'no' })
+                    }
+
+
+                })
+
+            }
+
+        })
+        res.send({ status: 'no' })
+
+    } else {
+        if (fn.clients.length == 0) {
+            res.send({ status: 'no' })
+        } else {
+            fn.sendSocketToSpeaker(req.body.key, 'pause_toggle', function(result) {
+                if (result != false) {
+                    res.send({ status: 'ok' })
+                } else {
+                    res.send({ status: 'no' })
+                }
+
+
+            })
+        }
+
+    }
+
+
+})
+
+
+
+
+function securityCheck(req, response, next) {
+
+    var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+
+    if (fullUrl == 'http://www.baidu.com/cache/global/img/gs.gif' || fullUrl == 'http://www.baidu.comhttp://www.baidu.com/cache/global/img/gs.gif') {
+        console.log('intrusion try')
+        response.end()
+    } else {
+        next()
+    }
+}
+
+module.exports = router;
